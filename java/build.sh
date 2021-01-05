bd4dc3321a94a31b108e0acb3bc5fc3f73da2a4a
liyf <1010345121@qq.com>
2021-01-05 15:54:25 +0800

    linux fix

--- java/build.sh
+++ java/build.sh
@@ -1,4 +1,7 @@
-cd /path/to/GmSSL/java
-gcc -shared -fPIC -Wall -I./jni -I /path/to/gmssl/include -L /path/to/gmssl/lib GmSSL.c -lcrypto -o libgmssljni.so
-java -Djava.library.path=/ GmSSL
+cd /gitrepo/GmSSL/java
+cp ../libcrypto.so  ../libcrypto.so.1.1  ../libssl.so  ../libssl.so.1.1 lib/
+gcc -shared -fPIC -Wall -I ./jni -I /gitrepo/GmSSL/include -L /gitrepo/GmSSL/lib GmSSL.c -lcrypto -o lib/libgmssljni.so
+rm org/gmssl/GmSSL.class
+javac org/gmssl/GmSSL.java
+java -Djava.library.path=./lib org.gmssl.GmSSL
 
